36faaf9bca4df04b6751ea8db970e88fe93add85
katrin_gorbach <e.v.horbach@gmail.com>
2025-06-01 15:24:50 +0200

    Added DataProvider for CheckoutTest

--- src/test/java/tests/CheckoutTest.java
+++ src/test/java/tests/CheckoutTest.java
@@ -1,14 +1,25 @@
 package tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class CheckoutTest extends BaseTest {
 
-	@Test(description = "Validation your information form", priority = 2, testName = "emptyYourInformation",
-			groups = {"regression"})
-	public void emptyYourInformation() {
+	@DataProvider(name = "Negative tests for Information")
+	public Object[][] loginData() {
+		return new Object[][]{
+				{"", "", "", "Error: First Name is required"},
+				{"", "", "4563", "Error: First Name is required"},
+				{"Katsiaryna", "", "", "Error: Last Name is required"},
+				{"Katsiaryna", "Horbach", "", "Error: Postal Code is required"}
+		};
+	}
+
+	@Test(dataProvider = "Negative tests for Information", description = "Validation your information form",
+			priority = 2, testName = "emptyYourInformation", groups = {"regression"})
+	public void emptyYourInformation(String firstName, String lastName, String zip, String errorMessage) {
 		loginPage.open();
 		loginPage.login("standard_user", "secret_sauce");
 		productPage.addProduct("Sauce Labs Backpack");
