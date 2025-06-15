91bde1c05978495afebe85c40af21f7f753d9a87
katrin_gorbach <e.v.horbach@gmail.com>
2025-06-16 01:17:39 +0200

    Updated credentials

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -28,7 +28,7 @@ public class LoginTest extends BaseTest {
 	@Description("Validation Login with empty Password")
 	public void checkLoginWithEmptyPassword() {
 		loginPage.open();
-		loginPage.login("standard_user", "");
+		loginPage.login(user, "");
 		//метод assertEquals импортирован статически из библиотеки JUnit или TestNG,
 		// что позволяет использовать его напрямую без указания класса.
 		assertEquals(loginPage.getErrorMessage(), "Epic sadface: Password is required",
@@ -39,8 +39,8 @@ public class LoginTest extends BaseTest {
 	@Description("Locator Validation")
 	public void checkLocator() {
 		driver.get("https://www.saucedemo.com/");
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		driver.findElement(By.id("user-name")).sendKeys(user);
+		driver.findElement(By.id("password")).sendKeys(password);
 		driver.findElement(By.id("login-button")).click();
 		String title = driver.findElement(By.cssSelector(".title")).getText();
 		assertEquals(title, "Products", "Логин не выполнен");
@@ -50,7 +50,7 @@ public class LoginTest extends BaseTest {
 	@Description("Login with wrong password")
 	public void checkLocatorInvalidPassword() {
 		driver.get("https://www.saucedemo.com/");
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		driver.findElement(By.id("user-name")).sendKeys(user);
 		driver.findElement(By.id("password")).sendKeys("secret_sauce1");
 		driver.findElement(By.id("login-button")).click();
 		String error = driver.findElement(By.xpath("//h3[@data-test='error']")).getText();
@@ -62,8 +62,8 @@ public class LoginTest extends BaseTest {
 	@DataProvider(name = "Negative tests for Login")
 	public Object[][] loginData() {
 		return new Object[][]{
-				{"standard_user", "", "Epic sadface: Password is required"},
-				{"standard_user", "123", "Epic sadface: Username and password do not match any user in this service"}
+				{user, "", "Epic sadface: Password is required"},
+				{user, "123", "Epic sadface: Username and password do not match any user in this service"}
 		};
 	}
 
