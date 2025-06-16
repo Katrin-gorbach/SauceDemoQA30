8c0ddd55f5a79a0c8130fb22a77ecd5316776162
katrin_gorbach <e.v.horbach@gmail.com>
2025-06-16 19:24:20 +0200

    updated BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -39,6 +39,10 @@ public class BaseTest {
 			options.addArguments("--disable-infobars");
 			options.addArguments("--headless");
 
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+			options.addArguments("--disable-gpu");
+
 			driver = new ChromeDriver(options);
 		} else if (browser.equalsIgnoreCase("edge")) {
 			EdgeOptions options = new EdgeOptions();
